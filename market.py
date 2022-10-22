acbe074754b3dc8ec398734f45925a8beca592a8
Tanimou Cisse <tanimou@live.fr>
2022-10-22 21:41:26 +0000

    routing

--- market.py
+++ market.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/market")
+def market():
+    return "<p>Market page</p>"
+
+@app.route("/market/<item>")
+def item(item):
+    return f"<p>Item page</p>{item}"
