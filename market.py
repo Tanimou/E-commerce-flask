1a300d2cff102f535b03c5945de0e91e836106a1
Tanimou Cisse <tanimou@live.fr>
2022-10-24 21:29:23 +0000

    Template inheritance in flask

--- market.py
+++ market.py
@@ -9,7 +9,8 @@ def home_page():
     return render_template("home.html")
 
 @app.route("/market/<name>")
-def market(name):
+@app.route("/market")
+def market():
     items=[{'id':1,'name':'Phone','barcode':'893212299897','price':500},
            {'id':2,'name':'Laptop','barcode':'123985473165','price':900},
            {'id':3,'name':'Keyboard','barcode':'231985128446','price':150}]
