cb3a04039647edebcb96b75d58411d590add4604
Tanimou Cisse <tanimou@live.fr>
2022-11-09 19:53:21 +0000

    Model reliattionship

--- market/models.py
+++ market/models.py
@@ -3,9 +3,21 @@
 #initialize the database
 from market import db
 
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(30),unique=True,nullable=False)
+    email_address=db.Column(db.String(50),unique=True,nullable=False)
+    password_hash=db.Column(db.String(60),nullable=False)
+    budget=db.Column(db.Integer,nullable=False,default=1000)
+    items=db.relationship('Item',backref='owned_user',lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email_address}')"
+
 class Item(db.Model):
     name=db.Column(db.String(30),unique=True,nullable=False)
     barcode=db.Column(db.String(12),unique=True,nullable=False)
     price=db.Column(db.Integer(),nullable=False)
     id=db.Column(db.Integer(),nullable=False,primary_key=True)
     description=db.Column(db.String(100),nullable=False)
+    owner=db.Column(db.Integer(),db.ForeignKey('user.id')) #this is the foreign key
--- market/routes.py
+++ market/routes.py
@@ -1,5 +1,5 @@
-from market import app
-from market.models import Item
+from market import app,db
+from market.models import Item,User
 from flask import render_template
 @app.route("/")
 @app.route("/home")
@@ -11,9 +11,25 @@ def home_page():
 @app.route("/market")
 def market():
     #the query.all() method returns a list of all the items in the database
-    items=Item.query.all()
+    # db.drop_all()
+    # db.create_all()
     #for g,h in enumerate(items):
      #   g=Item(name=h['name'],barcode=h['barcode'],price=h['price'],description=h['description'])
     #    db.session.add(g)
      #   db.session.commit()
+    # u1=User(username='cisse',password_hash='cisse',email_address='cisse@live.fr')
+    # db.session.add(u1)
+    # db.session.commit()
+    # i1=Item(name='laptop',barcode='123456789012',price=500,description='this is a laptop')
+    # i2=Item(name='phone',barcode='123456789013',price=300,description='this is a phone')
+    # db.session.add(i1)
+    # db.session.commit()
+    # db.session.add(i2)
+    # db.session.commit()
+    items=Item.query.all()
+    item1=Item.query.filter_by(name='laptop').first()
+    item1.owner=User.query.filter_by(username='cisse').first().id
+    db.session.add(item1)
+    db.session.commit()
+    # db.session.rollback()
     return render_template("market.html",name=items)
